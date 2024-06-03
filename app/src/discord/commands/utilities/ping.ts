9186bd13dc494d7a1d145241af3f0eb152277949
Hung Nguyen <nthungdev@gmail.com>
2024-06-03 13:47:32 -0400

    disable ping command

--- app/src/discord/commands/utilities/ping.ts
+++ app/src/discord/commands/utilities/ping.ts
@@ -1,10 +1,10 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { DiscordCommand } from '../../constants';
+// import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+// import { DiscordCommand } from '../../constants';
 
-export const data = new SlashCommandBuilder()
-  .setName(DiscordCommand.Ping)
-  .setDescription('Replies with Pong!');
+// export const data = new SlashCommandBuilder()
+//   .setName(DiscordCommand.Ping)
+//   .setDescription('Replies with Pong!');
 
-export async function execute(interaction: ChatInputCommandInteraction) {
-  await interaction.reply('Pong!');
-}
+// export async function execute(interaction: ChatInputCommandInteraction) {
+//   await interaction.reply('Pong!');
+// }
